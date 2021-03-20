06abf6afeda8bc56f5bd133bc7bfa42c2223336b
Daniel Rosel <danalves24@outlook.com>
2021-03-20 15:14:02 +0100

    router

--- web_interface/src/App.js
+++ web_interface/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { 
+    BrowserRouter as Router, 
+    Route, 
+    Link, 
+    Switch 
+} from 'react-router-dom'; 
+import Landing from './pages/landing.js'
 
 function App() {
   return (
-  	
+  	<Router>
+	  <Switch>
+	  	<Route exact path="/" component={Landing} />
+	  </Switch>
+	 </Router>
   );
 }
 
