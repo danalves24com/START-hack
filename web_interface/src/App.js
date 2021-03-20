92dcb1f70de136a3e4d232d7e826f7ae509ee999
Daniel Rosel <danalves24@outlook.com>
2021-03-20 17:24:39 +0100

    both company and employee signups

--- web_interface/src/App.js
+++ web_interface/src/App.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom'; 
 import Landing from './pages/landing.js'
 import cRegister from './pages/companyRegister.js'
+import Signup from './pages/signup.js'
 function App() {
   return (
   	<Router>
 	  <Switch>
 	  	<Route exact path="/" component={Landing} />
 	  	<Route exact path="/regc" component={cRegister} />
+	  	<Route exact path="/signup" component={Signup} />
 	  </Switch>
 	 </Router>
   );
--- web_interface/src/pages/companyRegister.js
+++ web_interface/src/pages/companyRegister.js
@@ -1,6 +1,30 @@
 import React, {Component} from 'react';
 import $ from 'jquery'
 
+function generateKey() {
+	var name = $("#name")[0].value;
+	if(name.length > 0) {
+		
+		var settings = {
+		  "url": "http://localhost:8000/add/company/"+name,
+		  "method": "GET",
+		  "timeout": 0,
+		};
+
+		console.log(name)
+
+		$.ajax(settings).done(function (response) {
+		  console.log(response);
+			if(response.status == "success") 
+			{
+				var id = response.payload.company_UID, link = window.location.origin+"/signup";
+				var sum = link+"?c="+id;
+				$("#linkToShare").html(sum)			
+				$("#linkToShare").attr("href", sum)						
+			}
+		});
+	}
+}
 
 class cRegister extends Component {
 
@@ -12,8 +36,8 @@ class cRegister extends Component {
 						<div class="py-10">
 							
 							<center>
-								<input type="text" class="border-black border-b" id="companyName" placeholder="company name"></input>
-								<button id="reg_btn" class="mx-4 roudned border-black border-2">Register</button>
+								<input id="name" type="text" class="border-black border-b" placeholder="company name"></input>
+								<button id="reg_btn" class="mx-4 roudned border-black border-2" onClick={generateKey}>Register</button>
 							</center>
 						</div>
 						<div class="py-5">
