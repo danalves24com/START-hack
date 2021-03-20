3309624d88b4a5102cb3bfe562a891dd31a936c4
Daniel Rosel <danalves24@outlook.com>
2021-03-20 14:53:03 +0100

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ var { Matcher } = require("./components/Matcher.js")
 var { companyAccountCreator } = require("./components/Signup-process.js")
 var {userSignup} = require("./components/userSignup.js");
 var pass = require("./config/pass.js")
+var ws = require("nodejs-websocket")
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -20,6 +21,80 @@ con.connect(function(err) {
 });
 
 
+class client {
+	constructor(con) {
+		this.con = con
+	}	
+	setUUID(uuid) {this.uuid = uuid;}
+	getUUID() {return this.uuid;}
+	getCON() {return this.con;}
+}
+
+
+/*
+ * WEBSOCKET
+ */
+
+var pool = {};
+
+var server = ws.createServer(function (conn) {
+	console.log("New connection")
+    	var cli = new client(conn)
+	conn.on("text", function (str) {
+		str = JSON.parse(str);
+		console.log(str);
+		switch(str['event']) {
+			case "id_self":				
+				cli.setUUID(str['data']['UUID']);
+				var id = cli.getUUID();
+				pool[`${id}`] = cli;
+				conn.sendText("helo "+id) // yes helo not hello
+				break;
+			case "send_to":
+				var id = str["data"]["to"], payload = str["data"]["from"];			
+				console.log("sending message to "+id);
+				var message = {"event":"com_rec", "data":{"origin":cli.getUUID(), "message":payload}}
+				var target = pool[id].getCON();
+				console.log(target);
+				target.sendText(JSON.stringify(message));
+//				((pool[id]).getCON()).sendText(JSON.stringify(message));
+				break;
+		}
+
+
+		/*
+		if(str.includes("id_self")) {
+
+		}
+		else if (str=="id_me") {
+			conn.sendText(cli.getUUID())
+		}
+		else if (str.includes("send_to")) {
+			var id = str.split(":")[1], payload = str.split(":")[2];			
+			console.log("sending message to "+id);
+			for(var e in pool) {
+				console.log(e)				
+			}
+			var message = {"event":"com_rec", "data":{"origin":cli.getUUID(), "message":payload}}
+			((pool[`${id}`]).getCON()).sendText(JSON.stringify(message));
+		}
+		else {
+
+		} 
+		*/
+		console.log("Received "+str)
+
+	    })
+	conn.on("close", function (code, reason) {
+        	console.log("Connection closed")
+		delete pool[cli.getUUID];
+	})
+}).listen(8001)
+
+
+/*
+ * ENDPOINTS
+ */
 
 app.use(express.json());
 app.get("/add/company/:name", (req, res) => {
@@ -42,14 +117,10 @@ app.get("/stat", (req, res) => {
 })
 
 
-app.get("/auth/:code", (req, res) => {
-
-})
-
 
 app.get("/match/:uuid/:companyCode", (req, res) => {
 	var m = new Matcher(req.params.uuid, req.params.companyCode);
-	res.send(m.match());
+	m.match(res)
 })
 
 
